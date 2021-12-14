53f20cda05a066548ec4c8a20728abce9ae7cd1f
Ahmed Hamza <81289670+ham-zax@users.noreply.github.com>
2021-12-14 21:00:56 +0530

    Added Ng prime

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const modules: never[] = [
+ 
+]
+
+@NgModule({
+  declarations: [],
+  imports: [...modules,
+    CommonModule
+  ],
+  exports: [
+    ...modules,
+    
+  ]
+})
+export class SharedModule { }
